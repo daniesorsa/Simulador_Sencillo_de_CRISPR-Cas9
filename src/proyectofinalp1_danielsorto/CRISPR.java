2ce676a53afcef9c9f98041d969a06b80dbeed36
DaniSor <daniel_sorto@unitec.edu>
2025-12-08 21:46:55 -0600

    Atributos mutacion actualizados y main con enfermedades añadido

--- src/proyectofinalp1_danielsorto/CRISPR.java
+++ src/proyectofinalp1_danielsorto/CRISPR.java
@@ -1,5 +1,20 @@
 package proyectofinalp1_danielsorto;
 public class CRISPR {
+    public static char complementoDeAdn(char base) {
+        // Usamos switch para la lógica de decisión clara
+        switch (Character.toUpperCase(base)) {
+            case 'A':
+                return 'T';
+            case 'T':
+                return 'A';
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            default:
+                return 'X'; // Retorna un indicador de error si no es A, T, C, o G
+        }
+    }
     // metodos
     //metodo donde recibe secuenciaActual y posicion y hace el corte
 }
